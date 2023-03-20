9efa68c5a8e56a6d03a223988f29730857203d0f
abdelilah Fakhour <abdelilah.fakhour@outlook.fr>
2023-03-20 17:00:10 +0100

    ajout menu burger + début code responsive

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
+  };
+
+
+var sidenav = document.getElementById("navbar");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
